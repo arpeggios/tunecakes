4ef082fe259eee717d9d87e7981f96e7bbb542a8
Jason <jvcmoore@gmail.com>
2015-01-30 19:37:02 -0500

    Refactored with Angular, added form validation, and added pages for several links.

--- scripts/app.js
+++ scripts/app.js
@@ -4,11 +4,52 @@
 	///////////////////////////////////
 	var app = angular.module('tunecakes', ['ui.bootstrap']);
 
+	var users = [];
+
 	///////////////////////////////////
 	//CONTROLLERS
 	///////////////////////////////////
 	app.controller('MainController', function(){
-		// I dunno what to put in here yet
+		// Can't think of any use for this.
+	});
+	app.controller('LoginController', function(){
+		//add users[] here. Eventually link to users JSON or DB
+		this.success = function(){
+			location.href='welcome.html';
+		}
+	});
+	app.controller('SearchController', function() {
+		// this.userInput = {};
+		this.success = function(){
+		location.href='search-results.html';
+		}
 	});
 
+	///////////////////////////////////
+	//DIRECTIVES
+	///////////////////////////////////
+	app.directive('logoBlock', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'logo-block.html'
+		};
+	});
+	app.directive('accountLogin', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'account-login.html'
+		};
+	});
+	app.directive('searchForm', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'search-form.html'
+		}
+	});
+	app.directive('siteDescription', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'site-description.html'
+		}
+	});
 })();
--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,8 @@ function windowResize() {
 	var height = $(window).height();
 	var width = $(window).width();
 
-	if (height > 540){
+	if (height > 540 && width > 767){
 		$('.above-fold').css({'height':height});
 		$('.above-fold-img').css({'height':height});
-		// $('.below-fold-1').css({'height':height});
-		// $('.below-fold-2').css({'height':height});
 	}
 }
